09e3d7332ddbe964b6a26e7e7b6e0de32f358f15
NachaSilva <110260545+NachaSilva@users.noreply.github.com>
2023-08-22 19:33:50 -0400

    Agregar flag stats

--- cli.js
+++ cli.js
@@ -6,20 +6,52 @@ const path = args[0];
 
 const options = {
   validate: args.includes("--validate"),
+  stats: args.includes("--stats"), // Use "--stats" instead of "stats"
 };
 
 mdLinks(path, options)
-  .then((links) => {
-    links.forEach((link) => {
-      if (options.validate) {
-        console.log(
-          `${path} ${link.href} ${link.ok ? "ok" : "fail"} ${link.status} ${link.text}`
-        );
-      } else {
-        console.log(`${path} ${link.href} ${link.text}`);
-      }
-    });
+  .then((result) => {
+    if (options.stats) {
+    if(options.validate){
+    const totalLinks = result.length;
+    const uniqueLinks = new Set(result.map((link)=> link.href)).size;
+    const brokenLinks = result.filter((link)=> !link.ok).length;
+    console.log(`Total: ${totalLinks}`);
+    console.log(`Unique: ${uniqueLinks}`);
+    console.log(`Broken: ${brokenLinks}`);
+    }else{
+      const totalLinks = result.length;
+      const uniqueLinks = new Set(result.map((link) => link.href)).size;
+      console.log(`Total: ${totalLinks}`);
+      console.log(`Unique: ${uniqueLinks}`);
+    }
+ } else {
+      result.forEach((link) => {
+        if (options.validate) {
+          console.log(
+            `${path} ${link.href} ${link.ok ? "ok" : "fail"} ${link.status} ${link.text}`
+          );
+        } else {
+          console.log(`${path} ${link.href} ${link.text}`);
+        }
+      });
+    }
   })
   .catch((error) => {
     console.log("Error", error.message);
   });
+
+// .then((links) => {
+//     links.forEach((link) => {
+//       if (options.validate) {
+//         console.log(
+//           `${path} ${link.href} ${link.ok ? "ok" : "fail"} ${link.status} ${link.text}`
+//         );
+//       } else {
+//         console.log(`${path} ${link.href} ${link.text}`);
+//       }
+//     });
+//   })
+//   .catch((error) => {
+//     console.log("Error", error.message);
+//   });
--- some/example.md
+++ some/example.md
@@ -13,6 +13,7 @@
   * [Array.prototype.map() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/map)
   * [Array.prototype.filter() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter)
   * [Array.prototype.reduce() - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce)
+  * [A - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce)
   * [Arreglos](curriculum.laboratoria.a/e/topics/javascript/04-arrays)
   
 </p></details>
